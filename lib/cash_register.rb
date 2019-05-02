8416aa01da31afd6514112848c86242bfe17cc83
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-02 03:47:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ attr_accessor :total, :discount, :items
   end
 
   def add_item(title, price, quantity=1)
-    @items << title*quantity
+    quantity.times {|title| @items << title}
     previous_total = @total + (price*quantity)
     @total += (price * quantity)
     @total
