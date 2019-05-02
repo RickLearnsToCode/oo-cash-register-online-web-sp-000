7ebc5aff24f8ccc6f6d0602c8a55fb96379122af
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-02 03:05:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
 
+class CashRegister 
+attr_accessor :total 
+
+
+  def initialize(employee_discount = 0)
+      @total = 0
+    end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += price
+  end
+  
