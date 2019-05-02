82a07b1cebdcde6f6116c2d4652f9adbb9dd56dc
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-02 03:42:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,7 +6,7 @@ attr_accessor :total, :discount, :items
 
   def initialize(employee_discount = 0)
       @total = 0
-      @discount = employee_discount / 100.0
+      @discount = employee_discount
       @items = []
     end
 
